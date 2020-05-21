697b6d00b2b4511090a6a21097494d0edee4fdfb
davidSospina <david.12.ospina@gmail.com>
2020-05-21 14:50:24 -0500

    first commit

---
+++ database/migrations/2020_05_21_190649_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->String('name',200);
+            $table->bigInteger('category_id');
+            $table->text('description',200);
+            $table->datetime('review_date');
+            $table->enum('state', ['publish', 'review', 'rejected'])->default('review');
+            $table->integer('author_id');
+            $table->String('archive_pdf');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
